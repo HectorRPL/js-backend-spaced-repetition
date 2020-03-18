3d58edc3613ef1efed0ceef6b5f8ef78778a045e
hector.kaizen@gmail.com <12345678!"#$%&/(>
2020-03-18 08:13:46 -0600

    130. Resolución de la tarea de la carga de imágenes

--- routes/upload.js
+++ routes/upload.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const fileUpload = require('express-fileupload');
 const app = express();
+
+// libs
+const fileUpload = require('express-fileupload');
+app.use(fileUpload({}));
+const fileSistem = require('fs');
+
+// models
 const usuario = require('../models/usuario');
 const medico = require('../models/medico');
 const hospital = require('../models/hospital');
-const fileSistem = require('fs');
-
-app.use(fileUpload({}));
 
 app.put('/:tipo/:usuarioId', (req, res) => {
 
@@ -35,10 +38,10 @@ app.put('/:tipo/:usuarioId', (req, res) => {
 
     const archivo = req.files['']; // que raro que esté difernte a lo que está en el curso
     const nombreCortado = archivo.name.split('.');
-    const expencionArchivo = nombreCortado[nombreCortado.length - 1];
+    const extencionArchivo = nombreCortado[nombreCortado.length - 1];
     const extencionesPermitidas = ['png', 'jpg', 'gif', 'jpeg'];
 
-    if (extencionesPermitidas.indexOf(expencionArchivo) < 0) {
+    if (extencionesPermitidas.indexOf(extencionArchivo) < 0) {
         return res.status(400).json({
             mensaje: 'Solo se permiten' + extencionesPermitidas.join(', ')
         });
@@ -46,7 +49,7 @@ app.put('/:tipo/:usuarioId', (req, res) => {
 
     // nombre de archivo personalizado
 
-    const nombreArchivo = `${usuarioId}-${new Date().getMilliseconds()}.${expencionArchivo}`;
+    const nombreArchivo = `${usuarioId}-${new Date().getMilliseconds()}.${extencionArchivo}`;
 
     // Mover el archivo del temporal a un path
 
@@ -67,11 +70,15 @@ app.put('/:tipo/:usuarioId', (req, res) => {
 
 });
 
-function subirPorTipo(tipo, usuarioId, nombreArchivo, res) {
+function subirPorTipo(tipo, id, nombreArchivo, res) {
 
     if (tipo === 'usuarios') {
 
-        usuario.findById(usuarioId, (err, usuarioEncontrado) => {
+        usuario.findById(id, (err, usuarioEncontrado) => {
+
+            if (!usuarioEncontrado) {
+                return res.status(400).json(err)
+            }
 
             const pathViejo = './uploads/usuarios/' + usuarioEncontrado.img;
 
@@ -115,8 +122,47 @@ function subirPorTipo(tipo, usuarioId, nombreArchivo, res) {
 
     if (tipo === 'medicos') {
 
-        medico.findById(usuarioId, (err, medico) => {
-            const pathViejo = './uploads/medicos/' + medico.img;
+        medico.findById(id, (err, medicoEncontrado) => {
+
+            if (!medicoEncontrado) {
+                return res.status(400).json(err)
+            }
+
+            const pathViejo = './uploads/medicos/' + medicoEncontrado.img;
+
+            // si existe elimina la imagen anterior
+            // console.log({pathViejo});
+            if (fileSistem.existsSync(pathViejo)) {
+
+                fileSistem.unlink(pathViejo, (err, res) => {
+
+                    if (err) {
+
+                        return res.status(500).json(err);
+
+                    }
+
+                    // console.log({res});
+
+                });
+
+            }
+
+            medicoEncontrado.img = nombreArchivo;
+
+            medicoEncontrado.save((err, medicoActualizado) => {
+
+                if (err) {
+
+                    return res.status(500).json(err)
+
+                }
+
+                return res.status(200).json(medicoActualizado);
+
+            });
+
+
         });
 
         return;
@@ -124,8 +170,47 @@ function subirPorTipo(tipo, usuarioId, nombreArchivo, res) {
 
     if (tipo === 'hospitales') {
 
-        hospital.findById(usuarioId, (err, hospital) => {
-            const pathViejo = './uploads/hospital/' + hospital.img;
+        hospital.findById(id, (err, hospitalEncontrado) => {
+
+            if (!hospitalEncontrado) {
+                return res.status(400).json(err)
+            }
+
+            const pathViejo = './uploads/hospitales/' + hospitalEncontrado.img;
+
+            // si existe elimina la imagen anterior
+            // console.log({pathViejo});
+            if (fileSistem.existsSync(pathViejo)) {
+
+                fileSistem.unlink(pathViejo, (err, res) => {
+
+                    if (err) {
+
+                        return res.status(500).json(err);
+
+                    }
+
+                    // console.log({res});
+
+                });
+
+            }
+
+            hospitalEncontrado.img = nombreArchivo;
+
+            hospitalEncontrado.save((err, hospitalActualizado) => {
+
+                if (err) {
+
+                    return res.status(500).json(err)
+
+                }
+
+                return res.status(200).json(hospitalActualizado);
+
+            });
+
+
         });
 
         return;

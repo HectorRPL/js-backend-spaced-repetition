ff84443610f2ddabf8377542ec0a43385574eec8
hector.kaizen@gmail.com <12345678!"#$%&/(>
2020-07-30 15:23:22 -0500

    ident

--- routes/list.js
+++ routes/list.js
@@ -4,97 +4,101 @@ const List = require('../models/list');
 const mdAutenticacion = require('../middelwares/autenticacion');
 
 // post
+
 app.post(
-    '',
-    mdAutenticacion.verificaToken,
-    (req, res) => {
+ '',
+ mdAutenticacion.verificaToken,
+ (req, res) => {
 
-    const list = new List({
-        userId: req.body.userId,
-        name: req.body.name,
-        description: req.body.description,
-        label: req.body.label
-    });
+   const list = new List({
+     userId: req.body.userId,
+     name: req.body.name,
+     description: req.body.description,
+     label: req.body.label
+   });
 
-    list.save((err, listCreated) => {
-        if (err) {
-            return res.status(500).json(err);
-        }
-        res.status(200).json(listCreated);
-    });
+   list.save((err, listCreated) => {
+     if (err) {
+       return res.status(500).json(err);
+     }
+     res.status(200).json(listCreated);
+   });
 
-});
+ });
 
 // gel all by user id
+
 app.get(
-    '/userId/:userId',
-    (req, res) => {
+ '/userId/:userId',
+ (req, res) => {
 
-    const QUERY = {
-        userId: req.params.userId
-    };
+   const QUERY = {
+     userId: req.params.userId
+   };
 
-    List.find(QUERY)
-        .skip(Number(req.query.desde) || 0) // apartir de aqui comienza a contar, si le mando un 10 entonces con el .limit() me trae los 15
-        .limit(1000) // solo envia 5 registros por cada petición
-        .exec((err, list) => { // TODO: aunque no le ponga props las manda, como la fecha.
+   List.find(QUERY)
+    .skip(Number(req.query.desde) || 0) // apartir de aqui comienza a contar, si le mando un 10 entonces con el .limit() me trae los 15
+    .limit(1000) // solo envia 5 registros por cada petición
+    .exec((err, list) => { // TODO: aunque no le ponga props las manda, como la fecha.
 
-            if (err) {
-                return res.status(500).json(err);
-            }
+      if (err) {
+        return res.status(500).json(err);
+      }
 
-            List.count({}, (err, count) => {
-                res.status(200).json({
-                        ok: true, // TODO: Aqui mejor ponemos la paginación
-                        list: list,
-                        rows: count
-                    }
-                );
-            });
+      List.count({}, (err, count) => {
+        res.status(200).json({
+           ok: true, // TODO: Aqui mejor ponemos la paginación
+           list: list,
+           rows: count
+         }
+        );
+      });
 
-        });
-});
+    });
+ });
 
 // get one
+
 app.get(
-    '/:listId',
-    mdAutenticacion.verificaToken,
-    (req, res) => {
-        List.findById(req.params.listId, (err, listFinded) => {
-            if (!listFinded) {
-                return res.status(404).json(err); // el usuario no existe
-            }
-            res.status(200).json(listFinded);
-        });
-    }
+ '/:listId',
+ mdAutenticacion.verificaToken,
+ (req, res) => {
+   List.findById(req.params.listId, (err, listFinded) => {
+     if (!listFinded) {
+       return res.status(404).json(err); // el usuario no existe
+     }
+     res.status(200).json(listFinded);
+   });
+ }
 );
 
 // put
+
 app.put(
-    '/list/:listId',
-    mdAutenticacion.verificaToken,
-    (req, res) => {
-        List.findById(req.params.id, (err, listFinded) => {
-            if (!listFinded) {
-                return res.status(404).json(err); // el usuario no existe
-            }
-            const newList = {
-                userId: listFinded.userId,
-                name: req.body.name,
-                description: req.body.description,
-                label: req.body.label,
-                created: listFinded.created,
-                update: new Date()
-            };
-            listFinded.save((err, newList) => {
-                if (err) {
-                    return res.status(500).json(err);  // error cualquiera
-                }
-                const usuarioToken = req.usuario;
-                res.status(200).json(newList);
-            });
-        });
-    }
+ '/list/:listId',
+ mdAutenticacion.verificaToken,
+ (req, res) => {
+   List.findById(req.params.id, (err, listFinded) => {
+     if (!listFinded) {
+       return res.status(404).json(err); // el usuario no existe
+     }
+     const newList = {
+       userId: listFinded.userId,
+       name: req.body.name,
+       description: req.body.description,
+       label: req.body.label,
+       created: listFinded.created,
+       update: new Date()
+     };
+     listFinded.save((err, newList) => {
+       if (err) {
+         return res.status(500).json(err);  // error cualquiera
+       }
+       const usuarioToken = req.usuario;
+       res.status(200).json(newList);
+     });
+   });
+ }
 );
 
 module.exports = app; // NUNCA OLVIDAR EXPORTAR. Propongo el siguinete procedimeinto:

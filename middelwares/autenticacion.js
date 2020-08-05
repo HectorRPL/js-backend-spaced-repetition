3dcbdf396774884639410a5b691fb41a75edcb34
hector.kaizen@gmail.com <12345678!"#$%&/(>
2020-08-05 14:04:53 -0500

    QF

--- middelwares/autenticacion.js
+++ middelwares/autenticacion.js
@@ -5,26 +5,20 @@ const SEED = require('../config/config');
 // TODO: cambiar a inglés
 
 exports.verificaToken = function (req, res, next) {
-
-  // console.log('LLEGO UN TOKEN: '); // req.header('user-token')
-
   let token = req.header('user-token');
 
   jwt.verify(token, SEED, (err, decoded) => {
-
-    console.log({'req.user': req.user});
-
-    if (err) {
-
-      return res.status(401).json(err);  // error usuario no autorizado (token incorrecto)
-
+    if (decoded === undefined) {
+      return res.status(401).json(err);
     }
-
+    const {email, _id} = decoded.user;
+    console.log(new Date(), _id, email);
+    if (err) {
+      return res.status(401).json(err);  // error usuario no autorizado (token incorrecto)
+    }
     req.user = decoded.user;
-
     next(); // necesario el next porque si no aquí se apra y no continua con lo demás
     // return res.status(200).json(decoded);  // decoded es el usuario logueado
-
   });
 };
 
